c211f22a1b1a15f339918f85bd6384615004232c
LeeJaeHyeok97 <hazardous10@naver.com>
2023-07-15 04:29:33 +0900

    마지막 줄 === 추가

--- src/Main.java
+++ src/Main.java
@@ -24,6 +24,7 @@ public class Main {
             System.out.println("==============");
             saleService.PayMethod(price, customer);
         }
+        System.out.println("==============");
 
     }
 }
